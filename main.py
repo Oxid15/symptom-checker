6321a0d9c79ee61e486c1010854c5477df6aaf6d
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-12-15 16:21:43 +0300

    Add initial files

---
+++ main.py
@@ -0,0 +1,26 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello")
+
+
+with open('token', 'r') as f:
+    TOKEN = f.readline()
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
