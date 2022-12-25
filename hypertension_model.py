1cfc7cd106dd68aa5f11930c36533230ac4821ce
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-12-25 22:58:08 +0300

    Fix syntax error

--- hypertension_model.py
+++ hypertension_model.py
@@ -11,7 +11,7 @@ class HypertensionModel:
         # [ 1, 2, 3 & 4, 5, 6, 7 & 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
         # The Ideal Scenario for Hypertension
-        HYPER_VECTOR = [ 2, 0,     2, 1, 2,     2, 2,  2,  1,  0,  1,  0,  1,  1,  0,  3,  3] - # Symptom Vector
+        HYPER_VECTOR = [ 2, 0,     2, 1, 2,     2, 2,  2,  1,  0,  1,  0,  1,  1,  0,  3,  3]  # Symptom Vector
         # [ 1,        1,  , 1,     1, 1,  1,                              2,  2] - # op 2
         # [                                                               1,  1] - # op 3
 
