66b43494a6d4ff46251689f973c5d8426b9efa35
Ash <92231544+maddoxx02@users.noreply.github.com>
2022-12-26 01:19:54 +0300

    Update model.py

--- model.py
+++ model.py
@@ -14,10 +14,14 @@ class SymptomChecker:
 
         hyper = self._hypertension_model.symptoms(user)
         diab = self._diabetes_model.symptoms(user)
-
-        if hyper:
-            return self._hypertension_model.recommendations(user)
-        elif diab:
-            return self._diabetes_model.recommendations(user)
+        
+        
+        
+        if hyper > diab:
+            return "Based on your Symptoms, Our Diagnosis suggests that you might be sufferring from Hypertension as you have a ", hyper,"% match to the criterias required to have Hypertension", "Please note this is a Diagnosis and not an Actual Pracitioners report, hence, If you feel the need, please consult a General Practitioner or Seek Emergency help As soon as possible.", "Here are a few recommendations on How to deal with Hypertension" , self._hypertension_model.recommendations(user)
+        elif diab < hyper:
+            return "Based on your Symptoms, Our Diagnosis suggests that you might be sufferring from Diabetes Type 2 as you have a ", hyper,"% match to the criterias required to have Type 2 Diabetes", "Please note this is a Diagnosis and not an Actual Pracitioners report, hence, If you feel the need, please consult a General Practitioner or Seek Emergency help As soon as possible.", "Here are a few recommendations on How to deal with Hypertension", self._diabetes_model.recommendations(user)
+        elif diab >80 and hyper >80:
+            return "Based on your Symptoms, Our Diganosis suggests that you might be sufferrring from Both hypertension & Diabetes Type 2 as you had a ", hyper,"% & ",diab, "% match to the criterias required to have Hypertension & Diabetes type 2 Respectively" 
         else:
-            return 'The system was not able to diagnose you with anything'  # We need to come up with some answer for this case also
+            return "Hmmm.... From the Symptoms you provided, it Seems though you might not be sufferring from either Hypertension or Diabetes. In case, your Symptoms start to get aggrevated over time, It is best to seek Medical Attenton as soon as possible."  # We need to come up with some answer for this case also
