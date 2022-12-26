f33cfe9f4d93e562999a2a55a25d52a5b8e7529b
arteme99 <83638610+arteme99@users.noreply.github.com>
2022-12-26 15:28:08 +0300

    Update model.py
    add diabetes type 1 and diabetes type 2

--- model.py
+++ model.py
@@ -13,15 +13,19 @@ class SymptomChecker:
             return "Unfortunately, This Symptom Checker is Specifically Designed for Adults above the age of 18. We recommend visiting a General Practitioner or A Pediatrician if its Urgent."
 
         hyper = self._hypertension_model.symptoms(user)
-        diab = self._diabetes_model.symptoms(user)
+        diab_type1 = self._diabetes_model.symptoms_type1(user)
+        diab_type2 = self._diabetes_model.symptoms_type2(user)
         
         
-        
-        if hyper > diab:
+        if hyper <0.3:
             return "Based on your Symptoms, Our Diagnosis suggests that you might be sufferring from Hypertension as you have a ", hyper,"% match to the criterias required to have Hypertension", "Please note this is a Diagnosis and not an Actual Pracitioners report, hence, If you feel the need, please consult a General Practitioner or Seek Emergency help As soon as possible.", "Here are a few recommendations on How to deal with Hypertension" , self._hypertension_model.recommendations(user)
-        elif diab < hyper:
+        elif diab_type2 < 0.3:
             return "Based on your Symptoms, Our Diagnosis suggests that you might be sufferring from Diabetes Type 2 as you have a ", hyper,"% match to the criterias required to have Type 2 Diabetes", "Please note this is a Diagnosis and not an Actual Pracitioners report, hence, If you feel the need, please consult a General Practitioner or Seek Emergency help As soon as possible.", "Here are a few recommendations on How to deal with Hypertension", self._diabetes_model.recommendations(user)
-        elif diab >80 and hyper >80:
+        elif diab_type2 < 0.3 and hyper <0.3:
             return "Based on your Symptoms, Our Diganosis suggests that you might be sufferrring from Both hypertension & Diabetes Type 2 as you had a ", hyper,"% & ",diab, "% match to the criterias required to have Hypertension & Diabetes type 2 Respectively" 
+        elif diab_type1 < 0.3:
+            return "Diabetes type1" 
+        elif diab_type1 < 0.3 and hyper <0.3:
+            return "Diabetes type1 and hypertension"
         else:
             return "Hmmm.... From the Symptoms you provided, it Seems though you might not be sufferring from either Hypertension or Diabetes. In case, your Symptoms start to get aggrevated over time, It is best to seek Medical Attenton as soon as possible."  # We need to come up with some answer for this case also
