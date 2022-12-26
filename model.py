3aeeda8e280f80a2de059b42cd99660e36114f91
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-12-26 16:34:42 +0300

    Add general report function

--- model.py
+++ model.py
@@ -8,17 +8,26 @@ class SymptomChecker:
         self._hypertension_model = HypertensionModel()
         self._diabetes_model = DiabetesModel()
 
+    def _report(self, disease: str, score: float) -> str:
+        return (
+                f'Based on your Symptoms, Our Diagnosis suggests that you might be sufferring'
+                f'from {disease} as you have a, {score * 100}% match to the criterias required'
+                f'to have {disease}'
+                f'\nPlease note this is a Diagnosis and not an Actual Pracitioners report, hence'
+                f'If you feel the need, please consult a General Practitioner or Seek Emergency'
+                f'help As soon as possible.\n Here are a few recommendations on How to deal'
+                'with {disease}\n')
+
     def check(self, user: UserModel) -> str:
         if user.age < 18:
             return "Unfortunately, This Symptom Checker is Specifically Designed for Adults above the age of 18. We recommend visiting a General Practitioner or A Pediatrician if its Urgent."
 
         hyper = self._hypertension_model.analysis(user)
-        
-        diab = 10
+
+        diab = 0
         #diab = self._diabetes_model.symptoms(user)
-        
-        
-        
+
+
         if hyper > diab:
             return "Based on your Symptoms, Our Diagnosis suggests that you might be sufferring from Hypertension as you have a ", hyper,"% match to the criterias required to have Hypertension", "Please note this is a Diagnosis and not an Actual Pracitioners report, hence, If you feel the need, please consult a General Practitioner or Seek Emergency help As soon as possible.", "Here are a few recommendations on How to deal with Hypertension" , self._hypertension_model.recommendations(user)
         elif diab < hyper:
@@ -26,4 +35,9 @@ class SymptomChecker:
         elif diab > 0.80 and hyper > 0.80:
             return "Based on your Symptoms, Our Diganosis suggests that you might be sufferrring from Both hypertension & Diabetes Type 2 as you had a ", hyper,"% & ",diab, "% match to the criterias required to have Hypertension & Diabetes type 2 Respectively" 
         else:
-            return "Hmmm.... From the Symptoms you provided, it Seems though you might not be sufferring from either Hypertension or Diabetes. In case, your Symptoms start to get aggrevated over time, It is best to seek Medical Attenton as soon as possible."  # We need to come up with some answer for this case also
+            return (
+                'Hmmm.... From the Symptoms you provided, it Seems though you might not be'
+                'sufferring from either Hypertension or Diabetes. In case, your Symptoms '
+                'start to get aggrevated over time, It is best to seek Medical Attenton'
+                'as soon as possible.'
+            )
